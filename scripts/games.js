e205f87c9177bc8cbfe278775b71b7a5d7ba59ea
pasaunders <kaorti@gmail.com>
2016-09-14 10:27:24 -0700

    added jquerey Ive got a list of games I like. I would like to figure out how to order them properly soon. I did a little too much cargo-cult coding. I used the blog projects code as a basis to work from. I completely understood everything I typed up, but I didnt think of it myself, so my learning isnt complete.

---
+++ scripts/games.js
@@ -0,0 +1,26 @@
+var games = [];
+
+function Game (dataEntry) {
+  this.name = dataEntry.name;
+  this.medium = dataEntry.medium;
+  this.genre = dataEntry.genre;
+  this.whyGood = dataEntry.whyGood;
+};
+
+Game.prototype.toHtml = function () {
+  var $newGame = $('section.template').clone();
+  $newGame.find('h3').text(this.name);
+  $newGame.find('p.medium').text(this.medium);
+  $newGame.find('p.genre').text(this.genre);
+  $newGame.find('p.whyGood').text(this.whyGood);
+  $newGame.removeClass('template');
+  return $newGame;
+};
+
+gameList.forEach(function(gameEntry) {
+  games.push(new Game(gameEntry));
+});
+
+games.forEach(function(gameEntry) {
+  $('#about').append(gameEntry.toHtml());
+});
