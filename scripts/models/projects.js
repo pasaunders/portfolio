4c6f6f6a11b159d32c36049fc408534dbd03d616
pasaunders <kaorti@gmail.com>
2016-09-28 11:11:42 -0700

    first shot at pulling data from my github and displaying it as projects.

---
+++ scripts/models/projects.js
@@ -0,0 +1,18 @@
+(function(module){
+  var projects = {}; //create a projects object to hold my projects downloaded from github
+  projects.allProjects = []; //create an array property to hold the list of projects
+
+  projects.requestProjects = function(timingCheck) {
+    $.ajax({
+      url: 'https://api.github.com/users/pasaunders/repos',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + githubToken},
+      success: function(data, message, xhr) {
+        projects.allProjects = data;
+        timingCheck();
+      }
+    });
+  };
+
+
+})(window);
---
+++ scripts/views/aboutview.js
@@ -0,0 +1,36 @@
+(function(module) {
+  var aboutView = {};
+
+  aboutView.moreLessHandler = function () {  //uses jQuery to allow the user to expand and contract articles by clicking a div, then changes the div to say more or less
+    $('article#projects').on('click', 'a.expand', function(event) {
+      var $anchor = $(this);
+      event.preventDefault();
+      $anchor.siblings('p.whyGood').toggle();
+      if ($anchor.hasClass('expanded')) {
+        $anchor.html('More &rarr;');
+        $anchor.removeClass('expanded');
+      }
+      else {
+        $anchor.html('Less &larr;');
+        $anchor.addClass('expanded');
+      }
+    });
+  };
+
+  // aboutView.filterHandler = function () { //incomplete handler for filtering articles based on medium and genre properties
+    // $('article#projects').children('section').not('.template').each(function() {
+    //   console.log(this);
+    //   var gameMedium, gameGenre, optionTag;
+    //   gameMedium = $(this).find('p.medium').text();
+    //   optionTag = '<option value="' + gameMedium + '">' + gameMedium + '</option>';
+    //   $('#medium-filter').append(optionTag);
+    //   gameGenre = $(this).find('p.genre').text();
+    //   optionTag = '<option value="' + gameGenre + '">' + gameGenre + '</option>';
+    //   $('#genre-filter').append(optionTag);
+    // });
+  // };
+
+  module.aboutView = aboutView;
+  aboutView.moreLessHandler();
+  // aboutView.filterHandler();
+})(window);
